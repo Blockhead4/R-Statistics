9f14fa0b4a418690fb73394428177d97dfe2fe95
blockhead4 <blockhead4@naver.com>
2019-06-25 18:00:42 +0900

    ch06

--- Chapter06/ch06_1_scripts.R
+++ Chapter06/ch06_1_scripts.R
@@ -1,7 +1,16 @@
-# 6장 준비
+# 6장. 가설검정
+# 6-1. 가설검정
 setwd("D:/Workspace-JWP/R_Data_Analysis/R-Statistics/Chapter06")
-data <- read.csv("data/2010_6th.csv")
+data <- read.csv("data/2010_6차_남자.csv")
 str(data)
 
+tmp <- subset(data, data$나이==7)
+height.p <- tmp$X104.키
 
+set.seed(9)
+height <- height.p[sample(length(height.p), 15)]
 height
+
+mean(height)
+sd(height)
+t.test(height, mu=1220)
